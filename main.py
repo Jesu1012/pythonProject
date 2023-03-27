3a2e6559d2b6118ab20bc3ad1e020a02d1d64ada
Jesus <coronel12jesus@gmail.com>
2023-03-27 11:37:41 -0500

    Commit_1_2

--- main.py
+++ main.py
@@ -1,8 +1,74 @@
+"""TODO : 4"""
+# Input
+"""
+#Basic
+input("Whats your name: ")
+"""
+"""name = input("Enter your name: ")
+age = int(input("Enter your name: ")) # int or float
+age = age + 1
+print(f"Hello {name}")
+print(f"You are {age} years old")
+"""
+"""length = float(input("Enter the length of a rectangle: "))
+width = float(input("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+area = length * width * height
+print(f"The volume is : {area} cm^3")
+"""
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${round(total,0)}") # Round decimals with respect to what you indicate: 0,1,2
 
-# TODO: 2
-age = 20
+"""TODO: 3"""
+# Casting Explicit and Implicit
+
+"""name = 'Jesus'
+age = 0
+gpa = 1.9
+student = True
+"""
+"""#type
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
+"""
+# Explicit
+"""
+age = float(age)
+print(type(age)) #Response: <class 'float'>
+"""
+"""gpa = int(gpa)
+print(gpa) #Response: 1
+"""
+"""student = str(student)
+print(student)
+print(type(student)) #Response:<class 'str'>
+"""
+"""age = bool(age)
+print(age) #age!=0 <> true  <::> age = 0 <> false
+"""
+"""name = bool(name)
+print(name) # age!="" <> true  <::> age = "" <> false
+"""
+# Implicit
+"""x = 2
+y = 2.0
+
+x = x / y
+print(x) #Response: 1.0
+"""
+
+"""TODO: 2"""
+# Variables
+
 """Print 2
-    # print("I am  " + age +" years old")
+    age = 20
+    # print("I am  " + age +" years old")❌
     # print("I am "+str(age)+" years old")
     # print("I am", age, "years old")
     # print(f"I am {age} years old")
@@ -36,7 +102,6 @@ print(f"Hello {name}")
 print(f"You like {food}")
 print(f"Your email is:{email}")
 """
-
 """
 #Boolean
 
@@ -49,16 +114,23 @@ if running :
 else:
     print("The game is over")
 """
+"""
+#tips
+# x,y,z = 1,2,3
+# x = y = z = 1
+# print(x)
+# print(y)
+# print(z)
+"""
 
 
 
-# TODO: 1
+"""TODO: 1"""
+# Introductory
 """
 Print 1
     # print("My Cat is beautiful")
     # print("That Good")
 """
-
-
 
 #Documentation Python
